f142c3eb81cbb7df2b16455dab9a825b8e8f88e7
Narutich <skybest132@gmail.com>
2023-02-28 17:38:14 +0200

    second-commit

---
+++ src/main/java/com/example/ezjob/Test.java
@@ -0,0 +1,4 @@
+package com.example.ezjob;
+
+public class Test {
+}
---
+++ src/test/java/com/example/ezjob/EzJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ezjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
